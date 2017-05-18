7fe14234b94fc7f31225c381af8db44e32043a44
sagi <contact@sagishutrit.com>
2017-05-18 14:57:24 +0200

    Update DefaultController.php
    removed check route method

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -13,17 +13,11 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+        
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ));
     }
 
-    /**
-     * @Route("/Hi", name="test greet")
-     */
-    public function hiAction() {
 
-        die('it is nice');
-    }
 }
